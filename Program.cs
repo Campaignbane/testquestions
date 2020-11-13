baad72243c665ce971db0c2c2441ddbe40effd21
Campaignbane <68878871+Campaignbane@users.noreply.github.com>
2020-11-12 19:29:44 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestQuestion
+public class test1  
+{  
+  public static void Main(string) args)
+   {
+     Console.WriteLine(SumTriple(9,9));
+     Console.WriteLine(SumTriple(17,11));
+     Console.WriteLine(SumTriple(-8,9));
+   }
+    public static int SumTriple(int a, int b)
+   
+   {
+    return a == b ? (a + b) * 1 : a + b;
+  }
+}
