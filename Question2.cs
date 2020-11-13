a6816a8025c43c6e98a05891082cbd006cb5328f
Campaignbane <68878871+Campaignbane@users.noreply.github.com>
2020-11-13 18:30:30 -0500

    first commit

---
+++ Question2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace quest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(30, 0));
+            Console.WriteLine(test(-45, 15));
+            Console.WriteLine(test(25, 30));
+            Console.WriteLine(test(23, 20));
+
+        }
+
+        public static bool test(int x, int y)
+        {
+            bool v = x == 30 || y == 30;
+            return v || (x + y == 30);
+        }
+    }
+}
