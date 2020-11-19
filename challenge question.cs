e5215827f722f1a06d0ac044e9e06c71f5a9641a
Campaignbane <68878871+Campaignbane@users.noreply.github.com>
2020-11-18 19:36:10 -0500

    Add files via upload

---
+++ challenge question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using namespace ; funnyString
+
+
+int main()
+{
+ 
+    int q;
+    cin >> q;
+    
+
+    for (
+        int q_itr = 0; 
+        q_itr < q; 
+        q_itr++)
+    {
+        string s;
+        
+        getline(cin, s);
+
+        string result = funnyString(s);
+
+        fout << result <<
+    }
+
+    return 0;
+}
